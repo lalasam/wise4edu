f20dbefda97210224585486a5b95b6e25ce3da42
greenbee3 <rhd0527@gmail.com>
2017-08-17 13:58:45 +0900

    delete some line
    Signed-off-by: greenbee3 <rhd0527@gmail.com>

--- library2.sql
+++ library2.sql
@@ -1,33 +1,15 @@
--- --------------------------------------------------------
--- 호스트:                          127.0.0.1
--- 서버 버전:                        10.1.21-MariaDB - mariadb.org binary distribution
--- 서버 OS:                        Win32
--- HeidiSQL 버전:                  9.4.0.5174
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
--- library 데이터베이스 구조 내보내기
-CREATE DATABASE IF NOT EXISTS `library` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `library`;
-
--- 테이블 library.admin 구조 내보내기
 CREATE TABLE IF NOT EXISTS `admin` (
   `admin_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `admin_name` varchar(50) DEFAULT NULL,
   `admin_password` varchar(50) DEFAULT NULL,
   `admin_email` varchar(50) DEFAULT NULL,
   `status` char(1) DEFAULT 'A',
-  PRIMARY KEY (`a_id`)
+  PRIMARY KEY (`admin_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
--- 테이블 library.book 구조 내보내기
+
 CREATE TABLE IF NOT EXISTS `book` (
   `book_id` int(11) NOT NULL AUTO_INCREMENT,
   `title_statement` varchar(50) NOT NULL,
@@ -55,8 +37,7 @@ CREATE TABLE IF NOT EXISTS `book_management` (
   CONSTRAINT `FK_book_management_book` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
--- 테이블 library.rental_management 구조 내보내기
+
 CREATE TABLE IF NOT EXISTS `rental_management` (
   `rental_id` int(11) NOT NULL AUTO_INCREMENT,
   `book_id` int(11) DEFAULT NULL,
@@ -70,8 +51,7 @@ CREATE TABLE IF NOT EXISTS `rental_management` (
   CONSTRAINT `FK_rental_management_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
--- 테이블 library.reservation 구조 내보내기
+
 --CREATE TABLE IF NOT EXISTS `reservation` (
 --  `reserve_id` int(11) NOT NULL AUTO_INCREMENT,
 --  `book_id` int(11) DEFAULT NULL,
@@ -84,8 +64,7 @@ CREATE TABLE IF NOT EXISTS `rental_management` (
 --  CONSTRAINT `FK_reservation_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
 -- ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
--- 테이블 library.user 구조 내보내기
+
 CREATE TABLE IF NOT EXISTS `user` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(50) NOT NULL,
@@ -97,7 +76,4 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
